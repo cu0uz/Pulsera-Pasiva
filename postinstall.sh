814ef175440d06567f04e48959c531f463991624
cu0uz <jgarcia.delolmo@outlook.com>
2020-06-16 11:39:10 +0200

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -9,7 +9,7 @@ echo Vamos a añadir los repositorios al sistema
 echo 
 sleep 2s
 echo -e '##GKSU\ndeb http://deb.debian.org/debian/ stretch main\ndeb-src http://deb.debian.org/debian/ stretch main\n##backports\ndeb http://ftp.debian.org/debian/ buster-backports main non-free\ndeb-src http://ftp.debian.org/debian/ buster-backports main non-free\n##multimedia\ndeb http://www.deb-multimedia.org buster main non-free\ndeb http://www.deb-multimedia.org buster-backports main\n##ACTUALIZACIONES DE KERNELdeb http://mirrors.kernel.org/debian/ buster-updates main contrib non-free\ndeb-src http://mirrors.kernel.org/debian/ buster-updates main contrib non-free\ndeb http://mirrors.kernel.org/debian buster-proposed-updates main contrib non-free\ndeb-src http://mirrors.kernel.org/debian buster-proposed-updates main contrib non-free\n##ACTIVACIÓN DE LOS REPOSITORIOS PRIVATIVOS EN DEBIAN\ndeb http://ftp.de.debian.org/debian/ buster main contrib non-free\ndeb-src http://ftp.de.debian.org/debian/ buster main contrib non-free\ndeb http://security.debian.org/ buster/updates main contrib non-free\ndeb-src http://security.debian.org/ buster/updates main contrib non-free\n##SPOTIFY\ndeb http://repository.spotify.com stable non-free\n##ANYDESK\ndeb http://deb.anydesk.com/ all main\n##SKYPE\ndeb https://repo.skype.com/deb stable main' >> /etc/apt/sources.list
-wget https://archive.deb-multimedia.org/pool/main/d/deb-multimedia-keyring/deb-multimedia-keyring_2016.8.1_all.deb
+wget https://downloads.sourceforge.net/project/programas-esenciales/deb-multimedia-keyring_2016.8.1_all.deb
 dpkg -i deb-multimedia-keyring_2016.8.1_all.deb
 rm deb-multimedia-keyring_2016.8.1_all.deb
 apt-get install curl
